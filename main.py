af3beced3c4687045f04a975c3968349d87998ed
Djekson <alekssyvorov29@gmail.com>
2024-01-27 09:17:30 +0200

    Version 3.0

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+class Student:
+    group = "C2011"
+    education = "STEP"
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print(Student.group, Student.education)
+st = Student(name="Oleg", age=15)
+print(f"ST = {st.group} ")
+st1 = Student(name="Anna", age=16)
+print(f"ST1 = {st1.group} ")
+st2 = Student(name="Sacha", age=14)
+print(f"ST2 = {st2.group}")
+print(f"Name st {st.name} {st.age}")
+print(f"Name st1 {st1.name} {st1.age}")
+temp = f"Name st2 {st2.name}{st2.age}"
+print(temp)
+print("Name st2", st2.name,st2.age)
---
+++ test.py
@@ -0,0 +1,23 @@
+class Films:
+    Duration = "long"
+    Episodes = "less than 3"
+    Plot = "preconceived"
+
+    def __init__(self):
+        self.name = "fight club"
+        self.year = "1999"
+        self.main_character = "Narrator"
+
+class Series:
+    duration = "short"
+    episodes = "more than 3"
+    plot = "made up on the fly"
+
+    def __init__(self):
+        self.name = "Simpsons"
+        self.year = "1989"
+        self.main_character = "Simpson Family"
+
+film_1 = Films()
+print(film_1.name)
+print(film_1.year)
