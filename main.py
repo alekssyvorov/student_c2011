b04b2558a8974a03ae00708a4db43b8cf34f8fcc
Djekson <alekssyvorov29@gmail.com>
2024-01-27 10:55:13 +0200

    Version 5.0

--- main.py
+++ main.py
@@ -1,21 +1,60 @@
+import random
+
+
 class Student:
-    group = "C2011"
-    education = "STEP"
 
-    def __init__(self, name, age):
+    def __init__(self, name):
         self.name = name
-        self.age = age
+        self.gladness = 50
+        self.progress = 0
+        self.alive = True
+
+    def to_study(self):
+        print("Time to study")
+        self.progress += 0.15
+        self.gladness -= 3
+
+    def to_sleep(self):
+        print("I will sleep!")
+        self.gladness += 3
+
+    def to_chill(self):
+        print("Rest time")
+        self.gladness += 5
+        self.progress -= 0.1
+
+    def is_alive(self):
+        if self.progress < -0.5:
+            print("Cast out....")
+            self.alive = False
+        elif self.gladness <= 0:
+            print("Depression...")
+            self.alive = False
+        elif self.progress > 5:
+            print("Passed externally...")
+            self.alive = False
+
+    def end_of_day(self):
+        print(f"Gladness = {self.gladness}")
+        print(f"Progress = {round(self.progress, 1)}")
+
+    def live(self, day):
+        d = "Day " + str(day) + " of " + self.name + " life"
+        print(f"{day:=^50}")
+        live_cube = random.randint(1, 3)
+        if live_cube == 1:
+            self.to_study()
+        elif live_cube == 2:
+            self.to_sleep()
+        elif live_cube == 3:
+            self.to_chill()
+        self.end_of_day()
+        self.is_alive()
 
 
-print(Student.group, Student.education)
-st = Student(name="Oleg", age=15)
-print(f"ST = {st.group} ")
-st1 = Student(name="Anna", age=16)
-print(f"ST1 = {st1.group} ")
-st2 = Student(name="Sacha", age=14)
-print(f"ST2 = {st2.group}")
-print(f"Name st {st.name} {st.age}")
-print(f"Name st1 {st1.name} {st1.age}")
-temp = f"Name st2 {st2.name}{st2.age}"
-print(temp)
-print("Name st2", st2.name,st2.age)
+name = input("Input name ")
+nick = Student(name=name)
+for i in range(1, 366):
+    if nick.alive == False:
+        break
+    nick.live(i)
